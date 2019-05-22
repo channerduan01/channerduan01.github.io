9aef82730035f7610c9ee5e40be8b31984a5afb8
xiuchan.dc <xiuchan.dc@antfin.com>
2019-05-22 23:28:27 +0800

    text

--- deep_learning/tensorflow_tutorial_text_classification.py
+++ deep_learning/tensorflow_tutorial_text_classification.py
@@ -3,11 +3,18 @@
 """
 Created on Wed May 22 13:11:04 2019
 
+Tensorflow keras tutorial - Text sentiment classification
+
+Reference:
+1. tf official tutorial
+https://www.tensorflow.org/tutorials/keras/basic_text_classification
+
+
 @author: channerduan
 """
 
 
-#%%
+#%% define the dependency
 
 from __future__ import absolute_import, division, print_function
 
@@ -19,11 +26,15 @@ import numpy as np
 print(tf.__version__)
 
 
-#%%
-
+#%% load the data
 imdb = keras.datasets.imdb
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+word_index = imdb.get_word_index()
 
+#%%
+
+
+print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 
 #%%
 
